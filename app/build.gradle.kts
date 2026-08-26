22051edbfcad4ba537c2f548adac99270a8d4d85
aswinsroy1 <aswinsroy1@example.com>
2026-08-26 09:17:47 -0700

    ci: configure apk splits and github actions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,15 @@ android {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
   }
+  splits {
+    abi {
+      isEnable = true
+      reset()
+      include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+      isUniversalApk = true
+    }
+  }
+
   buildFeatures {
     compose = true
     buildConfig = true
