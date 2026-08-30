a7e8e160cd74631292bba0834e7fed7fbef40cc6
aswinsroy1 <aswinsroy1@example.com>
2026-08-30 07:30:00 -0700

    UI: Change bottom navbar height to 70dp

--- app/src/main/java/com/aeswox/arcmusic/ReusableComponents.kt
+++ app/src/main/java/com/aeswox/arcmusic/ReusableComponents.kt
@@ -384,7 +384,7 @@ fun BottomNavigation(
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
             .fillMaxWidth()
-            .height(80.dp)
+            .height(70.dp)
             .clip(RoundedCornerShape(AppCornerRadius))
             .glassEffect(hazeState, tintTransparency, noiseFactor)
             .graphicsLayer {
